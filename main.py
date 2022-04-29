c4ec66010da37e5cc2dd219a8fee9cfe05f9f3a8
Trevor Wright <trevor.wright.710@gmail.com>
2022-04-28 19:41:33 -0600

    perfected screen recording

--- main.py
+++ main.py
@@ -5,12 +5,32 @@ import pyautogui
 from time import time
 import win32gui, win32ui, win32con
 
-def window_capture():
-    w = 1920 
-    h = 1080 
+# Setup 
+window_name = 'Minecraft* 1.18.1'
+hwnd = win32gui.FindWindow(None, window_name)
 
-    # hwnd = win32gui.FindWindow(None, windowname)
-    hwnd = None
+window_rect = win32gui.GetWindowRect(hwnd)
+w = window_rect[2] - window_rect[0] 
+h = window_rect[3] - window_rect[1] 
+
+border_pixels = 8
+titlebar_pixels = 30
+w = w - (border_pixels * 2)
+h = h - titlebar_pixels - border_pixels
+cropped_x = border_pixels
+cropped_y = titlebar_pixels
+
+
+# screenshot functions
+def list_open_windows():
+    def winEnumHandler( hwnd, ctx ):
+        if win32gui.IsWindowVisible( hwnd ):
+            print (hex(hwnd), win32gui.GetWindowText( hwnd ))
+
+    win32gui.EnumWindows( winEnumHandler, None )
+
+def window_capture():
+    
 
     # get the image window data
     wDC = win32gui.GetWindowDC(hwnd)
@@ -19,7 +39,7 @@ def window_capture():
     dataBitMap = win32ui.CreateBitmap()
     dataBitMap.CreateCompatibleBitmap(dcObj, w, h)
     cDC.SelectObject(dataBitMap)
-    cDC.BitBlt((0,0),(w, h) , dcObj, (0,0), win32con.SRCCOPY)
+    cDC.BitBlt((0,0),(w, h) , dcObj, (cropped_x, cropped_y), win32con.SRCCOPY)
 
     # save the screenshot
     signedIntsArray = dataBitMap.GetBitmapBits(True)
